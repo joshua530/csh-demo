9cd0620ab2f37f37de5a3c36a9f5f6c1d3ba950d
joshua530 <joshuaomari5@gmail.com>
2021-10-06 12:19:16 +0300

    Add request handler methods

--- Pages/Pizza.cshtml.cs
+++ Pages/Pizza.cshtml.cs
@@ -4,13 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pizza.Services;
 
 namespace Pizza.Pages
 {
     public class PizzaModel : PageModel
     {
+        [BindProperty]
+        public Pizza.Models.Pizza NewPizza { get; set; }
+        public List<Pizza.Models.Pizza> pizzas;
         public void OnGet()
         {
+            pizzas = PizzaService.GetAll();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                pizzas = PizzaService.GetAll();
+                return Page();
+            }
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
+
+        public string GlutenFreeText(Pizza.Models.Pizza pizza)
+        {
+            if (pizza.IsGlutenFree)
+                return "Gluten free";
+            return "Not gluten free";
         }
     }
 }
